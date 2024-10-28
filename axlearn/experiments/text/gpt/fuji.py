6e58a178cb0ab6fba095e7a4081da83f81def852
Erin Horning <{ID}+{username}@users.noreply.github.com>
2024-10-28 16:02:11 +0000

    decrease fsdp

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -279,7 +279,7 @@ def get_trainer_kwargs(
             learner_kwargs=dict(peak_lr=1.5e-4, weight_decay=0.1),
             max_sequence_length=max_sequence_length,
             # train_batch_size=train_batch_size,
-            train_batch_size=2048,
+            train_batch_size=1024,
             max_step=max_step,
             mesh_shape=mesh_shape_from_axes(fsdp=-1),
             mesh_rules=(
@@ -311,7 +311,7 @@ def get_trainer_kwargs(
                     ChainConfigModifier.default_config().set(
                         config_modifiers=[
                             MeshShapeModifier.default_config().set(
-                                mesh_shape=mesh_shape_from_axes(data=-1, fsdp=256)
+                                mesh_shape=mesh_shape_from_axes(data=-1, model=2, fsdp=128)
                             ),
                             RematSpecModifier.default_config().set(
                                 remat_policies={
